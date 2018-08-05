539d130f0cb3821789e689677bb22493dec5bd3f
ole <35845231+olegithub@users.noreply.github.com>
2018-08-05 15:49:10 -0500

    add App and ToDo files

---
+++ src/components/ToDo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render() {
+    return (
+      <li> A todo will go here too</li>
+    );
+  }
+}
+
+
+export default ToDo;
+
